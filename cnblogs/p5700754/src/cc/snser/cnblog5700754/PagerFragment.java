488c78887b6435286905ea4efe481870e6a0b069
snser <snsermail@gmail.com>
2016-08-07 23:43:12 +0800

    ADD cnblogs demo project ：ViewPagerFragment（p5700754）

---
+++ cnblogs/p5700754/src/cc/snser/cnblog5700754/PagerFragment.java
@@ -0,0 +1,13 @@
+package cc.snser.cnblog5700754;
+
+import android.support.v4.app.Fragment;
+
+public class PagerFragment extends Fragment{
+    
+    public void onPageIn() {
+    }
+    
+    public void onPageOut() {
+    }
+    
+}
